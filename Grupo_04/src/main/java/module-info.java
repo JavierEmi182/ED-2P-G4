75524426a8484ddc13a06cd7e450227a18f72928
JavierEmi182 <73973552+JavierEmi182@users.noreply.github.com>
2022-08-09 19:13:39 -0500

    Add files via upload

---
+++ Grupo_04/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module estructuradatos.grupo_04 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens estructuradatos.grupo_04 to javafx.fxml;
+    exports estructuradatos.grupo_04;
+}
