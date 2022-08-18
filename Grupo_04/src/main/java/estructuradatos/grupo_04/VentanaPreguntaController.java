aec36d9b75d374f0e8ca1d4d1dc8844d97583f07
eauria12 <eauria@espol.edu.ec>
2022-08-18 08:52:51 -0500

    VentanaPregunta

---
+++ Grupo_04/src/main/java/estructuradatos/grupo_04/VentanaPreguntaController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuradatos.grupo_04;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author ERWIN AURIA
+ */
+public class VentanaPreguntaController implements Initializable {
+
+    @FXML
+    private Button buttonSi;
+    @FXML
+    private Button buttonNo;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
